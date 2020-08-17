f8f945557ddff21e30512df0ef899726792049ed
Tim Mushen <t@getx.io>
2020-08-17 11:20:55 -0700

    init

---
+++ components/section.vue
@@ -0,0 +1,14 @@
+<template>
+	<section class="py-16" v-bind="$attrs">
+		<div class="ma-16">
+			<slot />
+		</div>
+	</section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
